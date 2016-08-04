b57b490a37e56cf76fbf7f85ff4fd862508eee48
Vladyan18 <Vladyan18@gmail.com>
2016-08-04 21:15:40 +0600

    Добавьте файлы проекта.

---
+++ MGconnectors/ClientConnector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MGconnectors
+{
+    public class ClientConnector
+    {
+
+        private TcpClient clientSocket = new TcpClient();
+        private UdpClient broadcastingClient;
+        private receivingDelegate receivingFunc;
+
+        public delegate void receivingDelegate();
+
+        public ClientConnector()
+        {
+            try
+            {
+                broadcastingClient = new UdpClient(10406);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+
+        public bool isConnected()
+        {
+            return (clientSocket.Connected);
+        }
+
+        public void startConnecting(receivingDelegate receivingFunc)
+        {
+            this.receivingFunc = receivingFunc;
+            string hostName = System.Net.Dns.GetHostName();
+            System.Net.IPAddress broadcastIp = System.Net.Dns.GetHostEntry(hostName).AddressList[2];
+            string strIp = broadcastIp.ToString();
+            broadcastIp = System.Net.IPAddress.Parse(strIp.Substring(0, strIp.LastIndexOf(".") + 1) + 255);
+            new Thread(() => tryToReceiveIP(broadcastIp)).Start();
+        }
+
+        void tryToReceiveIP(System.Net.IPAddress broadcastIp)
+        {
+            while (!isConnected())
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes("Hello");
+                broadcastingClient.Send(bytes, bytes.Length, new System.Net.IPEndPoint(broadcastIp, 10408));
+                broadcastingClient.BeginReceive(getServerIp, new object());
+                Thread.Sleep(5000);
+                if (!isConnected()) broadcastingClient.Close();
+            }
+        }
+
+        private void getServerIp(IAsyncResult answer)
+        {
+            System.Net.IPEndPoint ip = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 10406);
+            byte[] bytes = broadcastingClient.EndReceive(answer, ref ip);
+            System.Net.IPAddress serverIp = System.Net.IPAddress.Parse(Encoding.ASCII.GetString(bytes));
+
+            connect(serverIp);
+        }
+
+        private void connect(System.Net.IPAddress serverIp)
+        {
+            clientSocket.Connect(serverIp, 10407);
+            if (isConnected()) receivingFunc.DynamicInvoke();
+        }
+    }
+}
---
+++ MGconnectors/ServerConnector.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGconnectors
+{
+    class ServerConnector
+    {
+        private TcpListener serverListener;
+        private UdpClient broadcastingClient = new UdpClient(10408);
+        private System.Net.IPAddress broadcastIp;
+        private System.Net.IPAddress serverIp;
+        private newClientDelegate callback;
+
+        public delegate void newClientDelegate(TcpClient newClient);
+
+        public ServerConnector()
+        {
+            try
+            {
+                broadcastingClient = new UdpClient(10408);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+
+        public void startListening(newClientDelegate callback)
+        {
+            this.callback = callback;
+
+            string hostName = System.Net.Dns.GetHostName();
+            serverIp = System.Net.Dns.GetHostEntry(hostName).AddressList[2];
+            string strIp = serverIp.ToString();
+            broadcastIp = System.Net.IPAddress.Parse(strIp.Substring(0, strIp.LastIndexOf(".") + 1) + 255);
+
+            serverListener = new TcpListener(serverIp, 10407);
+            serverListener.Start();
+            serverListener.BeginAcceptTcpClient(acceptingClient, new object());
+
+            broadcastingClient.BeginReceive(getHello, new Object());
+        }
+
+        void getHello(IAsyncResult hello)
+        {
+            System.Net.IPEndPoint ip = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 10408);
+            byte[] bHello = broadcastingClient.EndReceive(hello, ref ip);
+            if (Encoding.ASCII.GetString(bHello) == "Hello")
+            {
+                byte[] msg = Encoding.ASCII.GetBytes(serverIp.ToString());
+                broadcastingClient.Send(msg, msg.Length, new System.Net.IPEndPoint(broadcastIp, 10406));
+            }
+
+            broadcastingClient.BeginReceive(getHello, new Object());
+        }
+
+        void acceptingClient(IAsyncResult result)
+        {
+            TcpClient newClient = serverListener.EndAcceptTcpClient(result);
+            callback(newClient);
+            serverListener.BeginAcceptTcpClient(acceptingClient, new object());
+        }
+    }
+}
